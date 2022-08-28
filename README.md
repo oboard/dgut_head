f7dc1751d053a6d7fdc9d94592be967184a3019e
oboard <oboard@outlook.com>
2022-08-28 21:42:33 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# dgut_head
+通过https://freshman.dgut.edu.cn/freshman/student/
+网站漏洞可获取新生高考头像
---
+++ android/app/src/main/kotlin/com/example/dgut_head/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dgut_head
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
